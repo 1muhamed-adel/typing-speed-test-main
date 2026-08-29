785496bb5cdb8bdc899b001e163c5935ed6a9b14
1muhamed-adel <muhamed.adel.fci@gmail.com>
2026-08-29 22:27:37 +0300

    add loading spinner and edit result page

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,9 @@ let medBtn = document.getElementById("medium") as HTMLButtonElement;
 let hardBtn = document.getElementById("hard") as HTMLButtonElement;
 let words = document.getElementById("words") as HTMLElement;
 let time = document.getElementById("Time") as HTMLElement;
+let startDiv = document.getElementById('start-div') as HTMLDivElement
 let start = document.getElementById("start") as HTMLButtonElement;
+let startTitle =document.getElementById('start-title') as HTMLParagraphElement
 let timer = 60;
 let acc;
 time.textContent = String(timer);
@@ -69,16 +71,18 @@ function wpm(startTime: number, endTime: number) {
   const elapsedSeconds = (endTime - startTime) / 1000;
   const wpm = 0;
 }
-
-let startTime: number = Date.now();
+let startTime: number;
 let endTime: number;
 let currentLetter: Element | undefined;
 let typedChar: number = 0;
+let interval: any;
 function startGame() {
+  timer = 60;
+  startTime = Date.now();
   currentLetter = words.children[0];
   currentLetter?.classList.add("curser");
 
-  let interval = setInterval(() => {
+  interval = setInterval(() => {
     timer--;
     time.textContent = `0:${timer}`;
     if (timer <= 0) {
@@ -90,9 +94,14 @@ function startGame() {
 }
 
 function endGame(count: number) {
-  const countChar = count;
-  console.log(countChar);
   window.removeEventListener("keyup", handleTyping);
+  clearInterval(interval);
+  options.classList.replace("d-flex", "d-none");
+  result.classList.replace("d-none", "d-flex");
+  words.classList.add("d-none");
+  words.style = `filter : blur(4px)`;
+
+  resultPage();
 }
 
 function handleTyping(e: KeyboardEvent) {
@@ -114,33 +123,54 @@ function handleTyping(e: KeyboardEvent) {
   currentLetter?.classList.remove("curser");
   currentLetter = currentLetter?.nextElementSibling ?? undefined;
 
-  if (currentLetter?.nextElementSibling === undefined) {
+  if (!currentLetter) {
     endGame(typedChar);
+    return;
   }
 
   currentLetter?.classList.add("curser");
 }
 
+words.addEventListener('mouseup', () => {
+  words.style = `filter : blur(0)`;
+  startDiv.classList.add('d-none')
+  startGame();
+});
 start.addEventListener("click", () => {
   words.style = `filter : blur(0)`;
-  start.style = `display : none`;
+  startDiv.classList.add("d-none");
   startGame();
 });
 
+let options = document.getElementById("options") as HTMLDivElement;
+let result = document.getElementById("result") as HTMLDivElement;
 let resultImg = document.getElementById("result-img") as HTMLImageElement;
 let resultTitle = document.getElementById("result-title") as HTMLHeadingElement;
 let resultText = document.getElementById("result-text") as HTMLParagraphElement;
+let resultbtn = document.getElementById("restart") as HTMLButtonElement;
 
 function resultPage() {
   resultImg.src = "./assets/images/icon-completed.svg";
   resultTitle.style = `color : white`;
   resultTitle.textContent = "Test Complete!";
   resultText.innerText = "Solid run. Keep pushing to beat your high score.";
+
+  resultbtn.addEventListener("click", () => {
+    result.classList.replace("d-flex", "d-none");
+    options.classList.replace("d-none", "d-flex");
+    words.classList.replace("d-none", "d-block");
+    startDiv.classList.remove('d-none')
+    location.reload();
+  });
 }
-resultPage();
+
+let spinner = document.getElementById("loading") as HTMLDivElement;
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
+    spinner.classList.add("d-none");
+    startDiv.classList.remove("d-none");
+
     easyBtn.addEventListener("click", () => {
       easy(data);
       display(data, "easy");
@@ -151,6 +181,9 @@ fetch("./data.json")
     });
     hardBtn.addEventListener("click", () => {
       hard(data);
+      display(data, "hard");
+    });
+    resultbtn.addEventListener("click", () => {
       display(data, "hard");
     });
     hardBtn.click();
