aa08d5c85dad7de61c11f7fee726a7e6b8e4d279
1muhamed-adel <muhamed.adel.fci@gmail.com>
2026-09-01 19:55:03 +0300

    add accuracy and WPM

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,11 @@ let time = document.getElementById("Time") as HTMLElement;
 let startDiv = document.getElementById("start-div") as HTMLDivElement;
 let start = document.getElementById("start") as HTMLButtonElement;
 let startTitle = document.getElementById("start-title") as HTMLParagraphElement;
+let accuracy = document.querySelectorAll(".Accuracy");
+let WPM = document.querySelectorAll(".WPM");
+let totalChars: number;
+console.log(accuracy);
 let timer = 60;
-let acc;
 time.textContent = String(timer);
 
 function format(element: string) {
@@ -53,25 +56,35 @@ let display = (data: data, level: string) => {
 
 function easy(data: data) {
   let rondom = Math.floor(Math.random() * 10);
+  // console.log(data.easy.length)
   let essay: any = data.easy[rondom]?.text;
+  totalChars = essay.length;
   return essay?.split("");
 }
 
 function medium(data: data) {
   let rondom = Math.floor(Math.random() * 10);
   let essay: any = data.medium[rondom]?.text;
+  totalChars = essay.length;
   return essay?.split("");
 }
 
 function hard(data: data) {
   let rondom = Math.floor(Math.random() * 10);
   let essay: any = data.hard[rondom]?.text;
+  totalChars = essay.length;
   return essay?.split("");
 }
 
-function wpm(startTime: number, endTime: number) {
-  const elapsedSeconds = (endTime - startTime) / 1000;
-  const wpm = 0;
+function wpm() {
+  const elapsedSeconds = (performance.now() - startTime) / 1000;
+  console.log(startTime)
+
+  if (elapsedSeconds <= 0) return 0;
+
+  const elapsedMinutes = elapsedSeconds / 60;
+
+  return Math.round(typedChar / 5 / elapsedMinutes);
 }
 function disableBtn() {
   easyBtn.classList.add("disabled");
@@ -80,22 +93,19 @@ function disableBtn() {
   timeBtn.classList.add("disabled");
   passageBtn.classList.add("disabled");
 }
-// function enableBtn(){
-//    easyBtn.classList.remove('disabled')
-//   medBtn.classList.remove('disabled')
-//   hardBtn.classList.remove('disabled')
-// }
+
 let startTime: number;
 let endTime: number;
 let currentLetter: Element | undefined;
 let typedChar: number = 0;
 let interval: any;
 function startGame() {
+  startTime = performance.now();
   disableBtn();
-  timer = 60;
-  startTime = Date.now();
+  console.log(totalChars);
+
+  words.removeEventListener("mouseup", showWords);
   if (timeBtn.classList.contains("active")) {
-    console.log('dula')
     interval = setInterval(() => {
       timer--;
       time.textContent = `0:${timer}`;
@@ -104,6 +114,11 @@ function startGame() {
         endGame(typedChar);
       }
     }, 1000);
+  } else if (passageBtn.classList.contains("active")) {
+    interval = setInterval(() => {
+      timer++;
+      time.textContent = `${timer}S`;
+    }, 1000);
   }
   currentLetter = words.children[0];
   currentLetter?.classList.add("curser");
@@ -111,15 +126,18 @@ function startGame() {
 }
 
 timeBtn.addEventListener("click", () => {
+  timer = 60;
+  time.textContent = `${timer}`;
   timeBtn.classList.add("active");
   passageBtn.classList.remove("active");
-  time.classList.remove('d-none')
-
+  // time.classList.remove("d-none");
 });
 passageBtn.addEventListener("click", () => {
+  timer = 0;
+  time.textContent = `${timer}`;
   timeBtn.classList.remove("active");
   passageBtn.classList.add("active");
-  time.classList.add('d-none')
+  // time.classList.add("d-none");
 });
 
 function endGame(count: number) {
@@ -133,22 +151,39 @@ function endGame(count: number) {
   resultPage();
 }
 
+let correctChar: number = 0;
+let accuracyValue: number = 100;
+let keySound = new Audio("./assets/audio/key-press.mp3");
 function handleTyping(e: KeyboardEvent) {
-  if (
-    e.key === "Shift" ||
-    e.key === "Control" ||
-    e.key === "Alt" ||
-    e.key === "Meta"
-  ) {
+  if (e.key.length !== 1) {
     return;
   }
+  keySound.currentTime = 0;
+  keySound.play();
   typedChar++;
-  console.log(e.key);
+  WPM.forEach((Element)=>{
+    Element.textContent = `${wpm()}`
+  })
+
   if (e.key === currentLetter?.textContent) {
     currentLetter.classList.add("correct");
+    correctChar++;
   } else {
     currentLetter?.classList.add("incorrect");
   }
+
+  accuracyValue = Math.round((correctChar / typedChar) * 100);
+
+  accuracy.forEach((element) => {
+    if (accuracyValue > 98) {
+      element.classList.add("correct");
+    } else {
+      element.classList;
+    }
+
+    element.textContent = `${accuracyValue}%`;
+  });
+
   currentLetter?.classList.remove("curser");
   currentLetter = currentLetter?.nextElementSibling ?? undefined;
 
@@ -159,17 +194,13 @@ function handleTyping(e: KeyboardEvent) {
 
   currentLetter?.classList.add("curser");
 }
-
-words.addEventListener("mouseup", () => {
+function showWords() {
   words.style = `filter : blur(0)`;
   startDiv.classList.add("d-none");
   startGame();
-});
-start.addEventListener("click", () => {
-  words.style = `filter : blur(0)`;
-  startDiv.classList.add("d-none");
-  startGame();
-});
+}
+words.addEventListener("mouseup", showWords);
+start.addEventListener("click", showWords);
 
 let options = document.getElementById("options") as HTMLDivElement;
 let result = document.getElementById("result") as HTMLDivElement;
